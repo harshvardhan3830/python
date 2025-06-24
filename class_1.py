f07f80eb6d2f4efcf5f0fe00d6a8ff7c02ccae96
harshvardhan3830 <vardhan3830@gmail.com>
2025-06-24 22:02:54 +0530

    class 1

---
+++ class_1.py
@@ -0,0 +1,12 @@
+print("Hello world !!!")
+
+#this is comment
+
+name = 'Harshvardhan'  #str
+age = 26
+height = 5.11
+isStudent = False
+
+print("My name is " + name)
+print(f'My age is {age}') # formatted string
+print(f'My height is {height}')

0e4c58f01f16e0349c91b2ea93bc01aec83297e4
harshvardhan3830 <vardhan3830@gmail.com>
2025-06-25 21:33:14 +0530

    Class 2 File Upload

---
+++ class_2.py
@@ -0,0 +1,54 @@
+# inputs
+
+# name = input('Enter you name ====>>>> ' )
+# print("Welcome " + name + "✅")
+
+
+# x = str(5) # Type Casting
+# y = "hello"
+# z = False
+
+# print(type(x))
+
+# num = int(input("Enter a number"))
+# print(type(num))
+
+
+# Operators
+# Arithmatic Operators
+# +, - , *, /, //, %, **
+
+
+# x = 5
+# y = 2
+# result = 5 ** 2
+# print(f"Result is ======>>>>>>>> {result}")
+
+
+#Assignment Operators
+# =, +=, -=, *=, **=, %=
+
+# x = 5
+# y = x
+# y *= 3
+# # y = y - 3 
+
+# print(" Value of y is =====>>>>> ", y)
+
+#Comparsion Operators
+# ==, != , >, >= ,<=
+
+# x = 5
+# y = 5
+
+# print("Result is =====>>>> ", x != y)
+
+
+#Logical Operators
+# and, or, not
+# &&, ! , ||
+
+x = 5
+y = 2
+
+print("Result is ", not (x < y) or (y > x))

de3005175ad0fb33ca308c2778a24da1ce68cf6b
harshvardhan3830 <vardhan3830@gmail.com>
2025-06-26 21:27:48 +0530

    Class 3

---
+++ class_3.py
@@ -0,0 +1,38 @@
+# Conditional statements
+
+x=3
+y=3
+
+# if False :
+#     print ("This is true")
+# else :
+#     print ("X is less then Y")
+
+# if x > y:
+#     print("X is greater")
+# elif  x >= y:
+#     print("kuch toh")
+# elif x == y:
+#     print("X and Y are equal") 
+# else :
+#     print ("Y is greater")
+
+
+# number = -1
+
+# if number > 0:
+#     if number % 2 == 0:
+#         print("Number is even")
+#     else:
+#         print("Number is Odd")
+# else:
+#     print("Number is non positive value")
+    
+#Ternary Operator
+
+# Loops
+#For loop
+
+# for(i=0 ;i < 5; i++)
+for i in range(10):
+   print("Value of i is ==>>> ", 12 * (i+1) ) # 0,1,2,3,4
